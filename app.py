728762dd876e18d056295c154461ac52f8b4605b
Guyodub <guyodubjarso@gmail.com>
2021-05-07 10:38:57 +0300

    update app.py

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ import joblib
 
 
 
-df = pd.read_csv('data.csv')
-df = df[pd.notnull(df['content'])]
-df['category_id'] = df['category'].factorize()[0]
-from io import StringIO
-category_id_df = df[['category', 'category_id']].drop_duplicates().sort_values('category_id')
-category_to_id = dict(category_id_df.values)
-id_to_category = dict(category_id_df[['category_id', 'category']].values)
-# Features and Labels
-features = tfidf.fit_transform(df.content).toarray()
-labels = df.category_id
-from sklearn.model_selection import train_test_split
+# df = pd.read_csv('data.csv')
+# df = df[pd.notnull(df['content'])]
+# df['category_id'] = df['category'].factorize()[0]
+# from io import StringIO
+# category_id_df = df[['category', 'category_id']].drop_duplicates().sort_values('category_id')
+# category_to_id = dict(category_id_df.values)
+# id_to_category = dict(category_id_df[['category_id', 'category']].values)
+# # Features and Labels
+# features = tfidf.fit_transform(df.content).toarray()
+# labels = df.category_id
+# from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
@@ -33,12 +33,13 @@ def predict():
 	# LSV_swahili_model = open('LSV_Swahili_model.pkl','rb')
 	nb_swahili_model = open('nb1_Swahili_model.pkl','rb')
 	model = joblib.load(nb_swahili_model)
+	vectorizer = pickle.load(open("vector.pickel", "rb"))
 
 	if request.method == 'POST':
 		message = request.form['message']
 		data = [message]
 
-		vect = tfidf.transform(data).toarray()
+		vect = vectorizer.transform(data).toarray()
 		
 		my_prediction = model.predict(vect)
 		# my_prediction= id_to_category[my_prediction[0]]
